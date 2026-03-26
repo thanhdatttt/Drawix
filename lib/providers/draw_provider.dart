57e3c3197baedcc58cd2a9b159c933d382d0579c
thanhdatttt <tmaxzv@gmail.com>
2026-03-26 16:35:08 +0700

    update models and add draw provider

--- lib/providers/draw_provider.dart
+++ lib/providers/draw_provider.dart
@@ -1,5 +1,11 @@
+import 'package:drawix_app/models/circle.dart';
+import 'package:drawix_app/models/ellipse.dart';
+import 'package:drawix_app/models/line.dart';
+import 'package:drawix_app/models/rectangle.dart';
 import 'package:drawix_app/models/shape.dart';
+import 'package:drawix_app/models/square.dart';
 import 'package:flutter/material.dart';
+import '../models/point.dart';
 
 class DrawProvider extends ChangeNotifier {
   final List<Shape> _shapes = [];
@@ -10,13 +16,13 @@ class DrawProvider extends ChangeNotifier {
 
   // default toolbox
   ShapeType _selectedType = ShapeType.line;
-  Color _selectedColor = Colors.black;
+  Color _selectedStrokeColor = Colors.black;
   Color? _selectedFillColor;
   double _strokeWidth = 2.0;
 
   // toolbox getters
   ShapeType get selectedType => _selectedType;
-  Color get selectedColor => _selectedColor;
+  Color get selectedStrokeColor => _selectedStrokeColor;
   double get strokeWidth => _strokeWidth;
 
   // toolbox setters
@@ -25,11 +31,55 @@ class DrawProvider extends ChangeNotifier {
     notifyListeners();
   }
   set selectedColor(Color color) {
-    _selectedColor = color;
+    _selectedStrokeColor = color;
     notifyListeners();
   }
   set strokeWidth(double width) {
     _strokeWidth = width;
+    notifyListeners();
+  }
+
+  // draw logic
+  void startDrawing(Offset startPoint) {
+    // Factory Method
+    _currentShape = _createShape(startPoint, startPoint);
+    notifyListeners();
+  }
+
+  void updateDrawing(Offset endPoint) {
+    if (_currentShape != null) {
+      _currentShape!.endPoint = endPoint;
+      notifyListeners();
+    }
+  }
+
+  void endDrawing() {
+    if (_currentShape != null) {
+      _shapes.add(_currentShape!);
+      _currentShape = null;
+      notifyListeners();
+    }
+  }
+  
+  Shape? _createShape(Offset startPoint, Offset endPoint) {
+    switch (_selectedType) {
+      case ShapeType.point:
+        return Point(startPoint: startPoint, strokeColor: _selectedStrokeColor, strokeWidth: _strokeWidth);
+      case ShapeType.line:
+        return Line(startPoint: startPoint, endPoint: endPoint, strokeColor: _selectedStrokeColor, strokeWidth: _strokeWidth);
+      case ShapeType.square:
+        return Square(startPoint: startPoint, endPoint: endPoint, strokeColor: _selectedStrokeColor, strokeWidth: _strokeWidth, fillColor: _selectedFillColor);
+      case ShapeType.rectangle:
+        return Rectangle(startPoint: startPoint, endPoint: endPoint, strokeColor: _selectedStrokeColor, strokeWidth: _strokeWidth, fillColor: _selectedFillColor);
+      case ShapeType.ellipse:
+        return Ellipse(startPoint: startPoint, endPoint: endPoint, strokeColor: _selectedStrokeColor, strokeWidth: _strokeWidth, fillColor: _selectedFillColor);
+      case ShapeType.circle:
+        return Circle(startPoint: startPoint, endPoint: endPoint, strokeColor: _selectedStrokeColor, strokeWidth: _strokeWidth, fillColor: _selectedFillColor);
+      }
+  }
+
+  void clearCanvas() {
+    _shapes.clear();
     notifyListeners();
   }
 }
